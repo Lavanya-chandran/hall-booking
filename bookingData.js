e08b917a40e42039e356a7c89dfa3a6aa1c1e8f8
Lavanya-chandran <rclavanya04@gmail.com>
2024-09-12 12:32:52 +0530

    commit

---
+++ bookingData.js
@@ -0,0 +1,22 @@
+const bookingData = [
+    {
+      bookingId: 1,
+      customerName: "Hema",
+      date: "2024-07-29",
+      startTime: "07",
+      endTime: "12",
+      roomId: 1,
+      status: "confirm",
+    },
+    {
+      bookingId: 2,
+      customerName: "Sathish",
+      date: "2024-08-29",
+      startTime: "06",
+      endTime: "10",
+      roomId: 2,
+      status: "confirm",
+    },
+  ];
+  
+  module.exports = bookingData;
